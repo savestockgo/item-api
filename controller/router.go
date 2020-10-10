09199b9a796d09bdd302f9095d3696760f1bf9e6
Anderson Lira <anderson.lira@celfocus.com>
2020-10-10 18:51:59 +0100

    basic auth approach changed

--- controller/router.go
+++ controller/router.go
@@ -1,6 +1,9 @@
 package controller
 
 import (
+	"encoding/base64"
+	"net/http"
+	"strings"
 	"os"
 
 	"github.com/andersonlira/item-api/config"
@@ -19,18 +22,13 @@ func MapRoutes(e *echo.Echo) {
 	g.Use(middleware.CORSWithConfig(middleware.CORSConfig{
 		AllowOrigins: []string{"*"},
 		AllowHeaders: []string{echo.HeaderOrigin, echo.HeaderContentType, echo.HeaderAccept, echo.HeaderAuthorization, echo.HeaderContentType},
-		AllowMethods: []string{echo.GET, echo.HEAD, echo.PUT, echo.POST, echo.DELETE, echo.OPTIONS},
+		AllowMethods: []string{echo.OPTIONS, echo.GET, echo.HEAD, echo.PUT, echo.POST, echo.DELETE},
 	}))
 
-	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
-		key := os.Getenv("apikey")
-		secret := os.Getenv("apisecret")
-		if username == key && password == secret {
-			return true, nil
-		}
-		return false, nil
-	}))		
+	e.Use(BasicAuth)
 
+
+	g.OPTIONS("/item", getDefaultOptions)	
 	g.GET("/item", GetItemList)
 	g.GET("/item/:id", GetItemByID)
 	g.POST("/item", SaveItem)
@@ -41,4 +39,32 @@ func MapRoutes(e *echo.Echo) {
 	g.PATCH("/item/:id/price/lowest", LowestPrice)
 	g.PATCH("/item/:id/price/highest", HighestPrice)
 	g.PATCH("/item/:id/price/last", LastPrice)
+}
+
+func getDefaultOptions(e echo.Context) error {
+	return nil
+}
+
+// Process is the middleware function to enabled options
+func BasicAuth(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		if c.Request().Method == echo.OPTIONS || c.Request().Method == echo.HEAD {
+			next(c)
+			return nil
+		}
+        auth := strings.SplitN(c.Request().Header.Get("Authorization"), " ", 2)
+
+        if len(auth) != 2 || auth[0] != "Basic" {
+            return nil
+        }
+
+        payload, _ := base64.StdEncoding.DecodeString(auth[1])
+        pair := strings.SplitN(string(payload), ":", 2)
+
+        if len(pair) == 2 && pair[0] == os.Getenv("apikey") && pair[1] == os.Getenv("apisecret") {
+			next(c)
+            return nil
+		}
+		return c.JSON(http.StatusUnauthorized, nil)
+	}
 }
